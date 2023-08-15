0666001039a0313a92d72bcdd263c0c7291f08cb
IvanDS-OF <139669858+IvanDS-OF@users.noreply.github.com>
2023-08-15 02:54:34 +0000

    simulation code - Reviewing

--- code.py
+++ code.py
@@ -8,7 +8,7 @@ import numpy as np
 import time
 from matplotlib.widgets import Slider, Button
 
-# We start with the function to plot a square
+# We start with the function to plot a big square (just reference)
 def Squa_C(tras_x, tras_y, rot_z):
 	valor_en_x = 8
 	valor_en_y = 8
@@ -24,6 +24,7 @@ def Squa_C(tras_x, tras_y, rot_z):
 	S1 = np.dot(np.dot(P1, P2), C)
 	return (S1[0], S1[1])
 
+# Plot a second square (smaller) -> This is the one that will be manipulated with the sliders
 def Squa(tras_x, tras_y, rot_z):
 	valor_en_x = 3
 	valor_en_y = 3
@@ -39,9 +40,9 @@ def Squa(tras_x, tras_y, rot_z):
 	S1 = np.dot(np.dot(P1, P2), C)
 	return (S1[0], S1[1])
 
-#First, we must draw the figure like a subplot
+# First, we must draw the figure like a subplot
 fig, ax = plt.subplots()
-#we need to start ploting the initial values, square in 0, 0
+# we need to start ploting the initial values, square in 0, 0
 ax.plot(Squa(0, 0, 0)[0], Squa(0, 0, 0)[1], c="0.8")
 ax.plot(Squa(0, 0, 0)[0], Squa(0, 0, 0)[1], c="b")
 ax.plot(Squa_C(0, 0, 0)[0], Squa_C(0, 0, 0)[1], c="0.8")
@@ -58,7 +59,8 @@ slider_y_plot = plt.axes([0.1, 0.1, 0.8, 0.05])		#Dimensions
 slider_z_plot = plt.axes([0.1, 0.15, 0.8, 0.05])		#Dimensions
 			   #plt.axes([left, bottom, width, height])
 
-
+# Definition of the properties of the sliders 
+#                 label, name, value min, value max, initial value, value of the step
 slider_x = Slider(slider_x_plot, "T_x", valmin=-10, valmax=10, valinit=0, valstep=1)#Characteristics
 slider_y = Slider(slider_y_plot, "T_y", valmin=-10, valmax=10, valinit=0, valstep=1)#Characteristics
 slider_z = Slider(slider_z_plot, "R_z", valmin=-90, valmax=90, valinit=0, valstep=1)#Characteristics
@@ -94,13 +96,13 @@ def update_z(val_z):
 
 
 
-#We write the command to update the plot, like that: 
+# We write the command to update the plot, like that: 
 slider_x.on_changed(update_x)
 slider_y.on_changed(update_y)
 slider_z.on_changed(update_z)
 
 
 
-
+# We should define the axis. 
 ax.axis([-10, 10, -10, 10])
 plt.show()
